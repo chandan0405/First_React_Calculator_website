83d7bf4a64967850a74ccc36c17d2c2d030a9e12
Chandan Kumar <chandan.kumar@intelliswift.com>
2023-06-30 20:58:34 +0530

    First React Website

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import fontawesome from "@fortawesome/fontawesome";
+import "./index.css";
+import * as math from "mathjs";
+import moonIcon from "./assets/images.png";
+import sunIcon from "./assets/sun2.png";
+import Header from "./Components/Header";
+import Keypad from "./Keypad/Keypad";
+
+const usedKeyCodes = [
+  48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103,
+  104, 105, 106, 107,108,109,111, 8, 13, 187, 189, 190,
+];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operator = ["-", "+", "*", "/"];
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false); //Dark Mode
+  const [expression, setExpression] = useState(""); //To handel Expression
+  const [result, setResult] = useState(""); //To handel result
+  const [history,setHistory]=useState([])
+
+  const HandelkeyPress = (keyCode, key) => 
+  {
+    //when key will be pressed
+    if (!keyCode) return;
+    if (!usedKeyCodes.includes(keyCode)) return;
+
+    if (numbers.includes(key)) 
+    {
+      if (key === "0" && expression.length === 0)return;
+        calculateResult(expression+key);
+       setExpression(expression + key);
+    } 
+    else if (operator.includes(key)) 
+    {
+      if (!expression) return;
+
+      const lastChar = expression.slice(-1);
+      if (operator.includes(lastChar)) return;
+      if (lastChar === '.') return;
+      setExpression(expression + key);
+
+    }
+     else if (key === '.')
+    {
+      if (!expression) return;
+      const lastChar = expression.slice(-1);
+      if (!numbers.includes(lastChar)) return;
+      setExpression(expression + key);
+
+    }
+     else if (keyCode === 8) 
+    {
+      if (!expression) return;
+      calculateResult(expression.slice(0,-1))
+      setExpression(expression.slice(0, -1));
+
+    } 
+    else if (keyCode === 13) 
+    {
+      if (!expression) return;
+      calculateResult(expression);
+      const tempHistory=[...history];
+      if(tempHistory.length>20)
+       tempHistory=tempHistory.splice(0,1);
+
+      tempHistory.push(expression);
+      setHistory(tempHistory);
+    }
+  };
+const calculateResult = (exp) => {
+  if (!exp) return;
+
+  let result;
+
+  try {
+    result = math.evaluate(exp).toFixed(2) + "";
+  } catch (error) {
+    result = "Error";
+  }
+
+  setResult(result);
+};
+
+  // const calculateResult = (exp) => {
+  //   if (!exp) return;
+
+  //   const lastChar = exp.slice(0, -1);
+  //   if (!numbers.includes(lastChar)) 
+  //     exp = exp.slice(0, -1);
+
+  //   const finalAnswer = eval(exp).toFixed(2) + "";
+  //   setResult(finalAnswer);
+  // }
+
+  return (
+    <>
+      <div
+        className="app"
+        tabIndex="0"
+        onKeyDown={(event) => HandelkeyPress(event.keyCode, event.key)}
+        data-theme={isDarkMode ? "dark" : " "}
+      >
+        <div className="app_calculator">
+          <div className="app_calculator_navbar">
+            <div
+              className="app_calculator_navbar_toggle"
+              onClick={() => setIsDarkMode(!isDarkMode)}
+            >
+              <div
+                className={`app_calculator_navbar_toggle_circle ${
+                  isDarkMode ? "app_calculator_navbar_toggle_circle_active" : ""
+                }`}
+              ></div>
+            </div>
+            <img src={isDarkMode ? moonIcon : sunIcon} alt="mode" />
+          </div>
+          <Header expression={expression} result={result} history={history} />
+          <Keypad HandelkeyPress={HandelkeyPress} />
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react'
+import './Header.css';
+import { expression } from 'mathjs';
+
+function Header(props) {
+  const resultRef=useRef();
+  const expressionRef=useRef();
+  useEffect(()=>
+  {
+    resultRef.current.scrollIntoView({behavior:"smooth"});
+  },[props.history]);
+
+  useEffect(()=>
+  {
+    expressionRef.current.scrollLeft = expressionRef.current.scrollWidth;
+  },[props.expression]); 
+
+  return (
+    <div className="header custom-scroll">
+      <div className="header_history">
+        {props.history &&
+          props.history.map((item) => (
+            <p key={item + "" + Math.random() * 44}>{item}</p>
+          ))}
+      </div>
+      <br></br>
+      <div ref={expressionRef} className="header_expression custom-scroll">
+        <p>{props.expression}</p>
+      </div>
+      <p ref={resultRef} className="header_result">
+        {props.result}
+      </p>
+    </div>
+  );
+}
+
+export default Header;
